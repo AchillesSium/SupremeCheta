4d6a548d12fade4f74865af118ce559a09e60948
Mohammad Aqib Hassan <h_aqib11@rocketmail.com>
2025-03-15 20:20:58 +0200

    connect login and register to auth controller is done

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const BASE_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 console.log('API URL:', BASE_API_URL); // Log the API URL being used
 
